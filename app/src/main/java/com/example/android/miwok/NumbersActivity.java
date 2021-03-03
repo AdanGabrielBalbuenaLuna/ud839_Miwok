ac3b3f04fd954c20116b9e3a86d7fe7c217aea00
Adan-Gabriel-Balbuena-Luna <agbalbuenaluna@gmail.com>
2021-03-02 20:47:56 -0600

    Module 2 class 20 -Grid Layout Comment

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,21 +47,25 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
 
-        //Catch Linear Layout by ID and
-        //Store in a LinearLayout object
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        for (int index = 0; index < words.size(); index++){
-            //Create a TextView Object to
-            //create child for LinearLayout
-            TextView wordView = new TextView(this);
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+        //GridView gridView = (GridView) findViewById(R.id.list);
 
-            //Use the "wordView" (TextView Object) to set Text into the TextView
-            //using the ArrayList we get the index 0 and store it
-            wordView.setText(words.get(index));
-
-            //Use the LinearLayout object to create a child
-            rootView.addView(wordView);
-        }
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
+        //gridView.setAdapter(itemsAdapter);
     }
 }
